5dca84d8423d86dfc103041613a27d1ca2196bec
daniele carlucci <carlucci.daniele@gmail.com>
2020-12-11 09:47:51 +0100

    Update app.js

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -393,9 +393,5 @@ app.filter('filtraSquadra', function($rootScope){
 	  }    
 	});
 app.config ["$httpProvider", ($httpProvider) ->
-csrfToken = $('meta[name=csrf-token]').attr('content')
-$httpProvider.defaults.headers.post['X-CSRF-Token'] = csrfToken
-$httpProvider.defaults.headers.put['X-CSRF-Token'] = csrfToken
-$httpProvider.defaults.headers.patch['X-CSRF-Token'] = csrfToken
-$httpProvider.defaults.headers.delete['X-CSRF-Token'] = csrfToken
+$httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content')
 ]
