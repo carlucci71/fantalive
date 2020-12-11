22ae1e62b13b8ee3c0f07207672d7daa09f38ebe
daniele carlucci <carlucci.daniele@gmail.com>
2020-12-11 12:59:58 +0100

    Update WebSecurityConfig.java

--- src/main/java/scrap/WebSecurityConfig.java
+++ src/main/java/scrap/WebSecurityConfig.java
@@ -1,15 +1,21 @@
 package scrap;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@EnableWebSecurity
 @Configuration
 	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	  @Override
 	  protected void configure(HttpSecurity http) throws Exception {
-	    http.requiresChannel()
+	    http
+	    .csrf().disable()	    .requiresChannel()
 	      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
 	      .requiresSecure();
+	    
 	  }
 	}
+
+
