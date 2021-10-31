67e48cc9df7943d679fd44e9158941183d304ba6
daniele carlucci <carlucci.daniele@gmail.com>
2021-10-31 17:50:25 +0100

    Update FantaCronacaLiveBOT.java

--- src/main/java/com/daniele/fantalive/bl/FantaCronacaLiveBOT.java
+++ src/main/java/com/daniele/fantalive/bl/FantaCronacaLiveBOT.java
@@ -1,29 +1,16 @@
 package com.daniele.fantalive.bl;
 
 import java.lang.reflect.Method;
-import java.net.InetAddress;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
-import org.checkerframework.framework.qual.InvisibleQualifier;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.meta.generics.BotSession;
 
-import com.daniele.fantalive.bl.Main.Campionati;
-import com.daniele.fantalive.model.Return;
-import com.daniele.fantalive.model.Squadra;
 import com.daniele.fantalive.util.Constant;
 
 public class FantaCronacaLiveBOT extends TelegramLongPollingBot{
