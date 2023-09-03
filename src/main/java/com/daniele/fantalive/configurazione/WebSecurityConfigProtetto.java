701c9e076cc4125f02bd6ad1f7440e988441fc6b
d.carlucci <carlucci.daniele@gmail.com>
2023-09-03 14:27:36 +0200

    Update WebSecurityConfigProtetto.java

--- src/main/java/com/daniele/fantalive/configurazione/WebSecurityConfigProtetto.java
+++ src/main/java/com/daniele/fantalive/configurazione/WebSecurityConfigProtetto.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @Configuration
@@ -41,8 +42,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
              .permitAll()
           .and()
           	  .csrf()
-              .disable();             
-    }	
+              .disable()             
+              .sessionManagement()
+              .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+              .sessionFixation().none();
+    	 }	
 	
 	}
 
