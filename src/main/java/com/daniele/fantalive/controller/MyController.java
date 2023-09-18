00024ea50fa684d4551d2ca8162ffa87b55e9e4f
d.carlucci <carlucci.daniele@gmail.com>
2023-09-18 20:19:04 +0200

    Update MyController.java

--- src/main/java/com/daniele/fantalive/controller/MyController.java
+++ src/main/java/com/daniele/fantalive/controller/MyController.java
@@ -205,7 +205,7 @@ public class MyController {
 		}
 		return ret;
 	}
-	@Scheduled(fixedRate = 10000)
+	@Scheduled(fixedRate = 1000)
 	public void chckNotifica() throws Exception {
 		if (constant.ABILITA_REFRESH) {
 	
