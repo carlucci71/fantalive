9206fc96b6f23d478ff8bbabe2df622593a51d61
d.carlucci <carlucci.daniele@gmail.com>
2023-09-20 08:51:53 +0200

    Update Main.java

--- src/main/java/com/daniele/fantalive/bl/Main.java
+++ src/main/java/com/daniele/fantalive/bl/Main.java
@@ -624,7 +624,7 @@ public class Main {
 		System.out.println("*******************MEMORY used: " + used_mem + " free:" + free_mem + " total: " + total_mem);
 		 */
 
-		System.out.println("SNAP");
+//		System.out.println("SNAP");
 		
 		timeRefresh=0;
 		Calendar c = Calendar.getInstance();
