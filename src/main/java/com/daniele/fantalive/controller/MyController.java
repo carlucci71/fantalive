62182f884bda059da28f444ac092fefe84660d75
d.carlucci <carlucci.daniele@gmail.com>
2023-09-27 00:16:02 +0200

    Update MyController.java

--- src/main/java/com/daniele/fantalive/controller/MyController.java
+++ src/main/java/com/daniele/fantalive/controller/MyController.java
@@ -703,6 +703,7 @@ public class MyController {
 		constant.GIORNATA=(Integer)body.get("giornata");
 		Main.oldSnapLives=null;
 		Main.oldSnapOrari=null;
+		Main.instantLetti=new HashMap<>();
 		Main.sqStatusMatch=new HashMap<>();
 
 		return ret;
