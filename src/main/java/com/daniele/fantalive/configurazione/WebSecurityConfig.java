b2a1c4e4c8ec98344f75fb596388dc9b2e58e1d9
d.carlucci <carlucci.daniele@gmail.com>
2025-09-17 11:55:39 +0200

    upd

--- src/main/java/com/daniele/fantalive/configurazione/WebSecurityConfig.java
+++ src/main/java/com/daniele/fantalive/configurazione/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.daniele.fantalive.configurazione;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @Order(100)
 @EnableWebSecurity
+@Profile(("!protetto"))
 public class WebSecurityConfig {
 
   @Bean
